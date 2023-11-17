5f36226e243c3ec76c931dd1f74510d631446973
Pedro Daniluz <pedrodaniluz04@gmail.com>
2023-11-17 15:33:11 -0300

    downloads

--- download_libs.py
+++ download_libs.py
@@ -1,8 +1,21 @@
 import os
 
-os.system("pip install requests")
-os.system("pip install datetime")
-os.system("pip install python-dotenv")
-os.system("pip install openai")
-os.system("pip install reportlab")
-os.system("pip install pypdf")
+# Caminho completo para o executável do Python
+python_executable = "python" if os.name == 'nt' else "python3"
+
+# Criação do ambiente virtual
+os.system(f"{python_executable} -m venv venv")
+
+# Ativação do ambiente virtual (verifique o sistema operacional)
+if os.name == 'nt':
+    os.system("venv\\Scripts\\activate")
+else:
+    os.system("source venv/bin/activate")
+
+# Certifique-se de que o ambiente virtual está ativado antes de instalar pacotes
+os.system(f"{python_executable} -m pip install requests")
+os.system(f"{python_executable} -m pip install datetime")
+os.system(f"{python_executable} -m pip install python-dotenv")
+os.system(f"{python_executable} -m pip install openai")
+os.system(f"{python_executable} -m pip install reportlab")
+os.system(f"{python_executable} -m pip install pypdf")

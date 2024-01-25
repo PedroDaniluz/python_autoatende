fa6cb16dc5086b4e2281aecb61d08c7cfcdcd925
Pedro Daniluz <pedrodaniluz04@gmail.com>
2024-01-25 10:54:54 -0300

    class implementation

--- functions.py
+++ functions.py
@@ -10,57 +10,59 @@ from pypdf import PdfWriter, PdfReader
 dotenv.load_dotenv()
 
 
-def limpar() -> None:
+def clear() -> None:
+    # Limpar script
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def formatar_sexo(sexo: str) -> str:
-    if sexo.upper() == "M":
-        return "Masculino"
-    else:
-        return "Feminino"
-
-
-def formatar_nome(nome: str) -> str:
+def name_format(name: str) -> str:
     # Captalizar as partes do nome
-    nome_split = nome.split(" ")
+    nome_split = name.split(" ")
     for i in range(len(nome_split)):
         nome_split[i] = nome_split[i].capitalize()
     return " ".join(nome_split)
 
 
-def validador_data(data: str) -> bool:
-    # Validar a data inserida
-    if len(data) == 8 and len(data.split("/")) == 1:
-        data = f"{data[:2]}/{data[2:4]}/{data[4:]}"
-    data_split = data.split("/")
+def gender_format(gender: str) -> str:
+    # Formatar gênero
+    if gender.upper() == "M":
+        return "Masculino"
+    else:
+        return "Feminino"
+
+
+def date_validator(date: str) -> bool:
+    # Validar a data inserida, tentando evitar exceptions
+    if len(date) == 8 and len(date.split("/")) == 1:
+        date = f"{date[:2]}/{date[2:4]}/{date[4:]}"
+    date_split = date.split("/")
     ano = datetime.now().year
-    if len(data_split) != 3:
+    if len(date_split) != 3:
         return False
     else:
-        if not data_split[0].isnumeric() or len(data_split[0]) != 2 or not 1 <= int(data_split[0]) <= 31:
+        if not date_split[0].isnumeric() or len(date_split[0]) != 2 or not 1 <= int(date_split[0]) <= 31:
             return False
-        elif not data_split[1].isnumeric() or len(data_split[1]) != 2 or not 1 <= int(data_split[1]) <= 12:
+        elif not date_split[1].isnumeric() or len(date_split[1]) != 2 or not 1 <= int(date_split[1]) <= 12:
             return False
-        elif not data_split[2].isnumeric() or len(data_split[2]) != 4 or not ano - 150 <= int(data_split[2]) <= ano:
+        elif not date_split[2].isnumeric() or len(date_split[2]) != 4 or not ano - 150 <= int(date_split[2]) <= ano:
             return False
         else:
             aux = True
             try:
-                datetime.strptime(data, "%d/%m/%Y")
+                datetime.strptime(date, "%d/%m/%Y")
             except ValueError:
                 aux = False
             return aux
 
 
-def consultar_cpf(cpf: str, nasc: str) -> dict:
-    if len(nasc) != 8 and len(nasc.split("/")) == 3:
-        nasc = f"{nasc[:2]}{nasc[3:5]}{nasc[6:]}"
+def cpf_query(cpf: str, bday: str) -> dict:
+    if len(bday) != 8 and len(bday.split("/")) == 3:
+        bday = f"{bday[:2]}{bday[3:5]}{bday[6:]}"
     # Consultar CPF e data de nascimento com a API da receita federal
     print("\nBuscando...")
     token = os.getenv("CPF_API_KEY")
     url = "https://www.sintegraws.com.br/api/v1/execute-api.php"
-    querystring = {"token": f"{token}", "cpf": cpf, "data-nascimento": f"{nasc}", "plugin": "CPF"}
+    querystring = {"token": f"{token}", "cpf": cpf, "data-nascimento": f"{bday}", "plugin": "CPF"}
 
     response = requests.request("GET", url, params=querystring)
 
@@ -68,10 +70,10 @@ def consultar_cpf(cpf: str, nasc: str) -> dict:
     if data['code'] == '0':
         return {
             'Situação': 'OK',
-            'Nome': formatar_nome(data['nome']),
+            'Nome': name_format(data['nome']),
             'CPF': f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}",
             'Idade': data['idade'],
-            'Sexo': formatar_sexo(data['genero'])
+            'Sexo': gender_format(data['genero'])
         }
     else:
         print(f"Erro encontrado: {data['message']}")
@@ -80,120 +82,139 @@ def consultar_cpf(cpf: str, nasc: str) -> dict:
         }
 
 
-def gpt_ask(pergunta: str) -> str:
-    # Mandar o request para o chatGPT
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+class Patient:
+    # Paciente só é criado após confirmar validade do CPF
+    # Criando um objeto Patient com valores padrões que são alterados ao longo do código
+    def __init__(self, cpf, name, age, gender, symp="", duration="", allergies="", medication="", suspicion="") -> None:
+        self.cpf: str = cpf
+        self.name: str = name
+        self.age: int = age
+        self.gender: str = gender
+        self.symp: str = symp
+        self.duration: str = duration
+        self.allergies: str = allergies
+        self.medication: str = medication
+        self.suspicion: str = suspicion
+        self.date: str = f"{datetime.now().date()}"
+        self.time: str = f"{datetime.now().time()}".split(".")[0]
 
-    resposta = client.chat.completions.create(model="gpt-3.5-turbo",
-                                              temperature=0.7,
-                                              messages=[
-                                                  {
-                                                      "role": "system",
-                                                      "content": "Você é um assistente virtual de um hospital, você "
-                                                                 "receberá a idade, o sexo, os sintomas, a duração "
-                                                                 "desses sintomas, as alergias e os medicamentos em uso"
-                                                                 " do paciente. Com base nisso, deve retornar apenas "
-                                                                 "uma string com as possiveis doenças separadas por "
-                                                                 "espaço e vírgula, sem avisos, nem introdução. Nem "
-                                                                 "titulo, apenas as três doenças captalizadas. Em PTBR"
-                                                                 "Por exemplo:"
-                                                                 "user: Dor de cabeça, dor nos olhos"
-                                                                 "return: Enxaqueca, Sinusite, Cansaço"
-                                                  },
-                                                  {
-                                                      "role": "user",
-                                                      "content": pergunta
-                                                  }
-                                              ])
-    return resposta.choices[0].message.content
+    def get_suspicion(self):
+        # Mandar o request para o chatGPT
+        # Armazena as suspeitas no atributo 'suspicion' do objeto declarado da classe Patient
+        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+        resposta = client.chat.completions.create(model="gpt-3.5-turbo",
+                                                  temperature=0.7,
+                                                  messages=[
+                                                      {
+                                                          "role": "system",
+                                                          "content": "Você é um assistente virtual de um hospital, "
+                                                                     "você receberá a idade, o sexo, os sintomas, a "
+                                                                     "duração desses sintomas, as alergias e os "
+                                                                     "medicamentos em uso do paciente. Com base nisso, "
+                                                                     "deve retornar apenas uma string com as possiveis "
+                                                                     "doenças separadas por espaço e vírgula, sem "
+                                                                     "avisos, nem introdução. Nem titulo, apenas as "
+                                                                     "três doenças captalizadas. Em PTBR\n"
+                                                                     "Por exemplo:\n"
+                                                                     "user: Dor de cabeça, dor nos olhos\n"
+                                                                     "return: Enxaqueca, Sinusite, Cansaço"
+                                                      },
+                                                      {
+                                                          "role": "user",
+                                                          "content": f"Idade: {self.age}\nSexo: {self.gender}\n"
+                                                                     f"Sintomas: {self.symp}\nDuração: "
+                                                                     f"{self.duration}\n Alergias: {self.allergies}\n "
+                                                                     f"Medicamento em uso: {self.medication}"
+                                                      }
+                                                  ])
+        self.suspicion = resposta.choices[0].message.content
+
+    def add_user_db(self):
+        # Carregar dados existentes do arquivo JSON
+        with open('aux/usuarios.json', 'r', encoding='utf-8') as arquivo:
+            data = json.load(arquivo)
+        # Adicionar novo usuário
+        new_user = {
+            'Nome': self.name,
+            'CPF': self.cpf,
+            'Idade': self.age,
+            'Sexo': self.gender,
+            'Sintomas': self.symp,
+            'Duração': self.duration,
+            'Alergias': self.allergies,
+            'Medicação': self.medication,
+            'Suspeitas': self.suspicion,
+            "Data de atendimento": self.date,
+            "Horário de atendimento": self.time
+        }
+
+        data.append(new_user)
+
+        # Salvar dados atualizados de volta ao arquivo JSON
+        with open('aux/usuarios.json', 'w', encoding='utf-8') as arquivo:
+            json.dump(data, arquivo, indent=4, ensure_ascii=False)
+
+    def create_pdf(self):
+        info = {
+            "Nome Completo": self.name,
+            "CPF": self.cpf,
+            "Idade": self.age,
+            "Sexo": self.gender,
+            "Sintomas": self.symp,
+            "Duração": self.duration,
+            "Alergia": self.allergies,
+            "Medicação em uso": self.medication,
+            "Suspeitas": self.suspicion,
+            "Data de atendimento": self.date,
+            "Horário de atendimento": self.time
+        }
+        pdf = canvas.Canvas(f"Paciente - {self.name}.pdf")
+        countlines = 0
+        # Escrevendo primeiro pdf:
+        for element in info:
+            pdf.drawString(100, 700 - countlines, f"{element}: {info[element]}")
+            countlines += 20
+        pdf.save()
+        # Trabalhando com segundo pdf:
+        stamp = PdfReader("aux/template.pdf").pages[0]
+        writer = PdfWriter(clone_from=f"Paciente - {self.name}.pdf")
+        for page in writer.pages:
+            page.merge_page(stamp, over=False)
+        writer.write(f"Paciente - {self.name}.pdf")
+        print("Ficha PDF gerada com sucesso.")
 
 
-def create_pdf(lista):
-    informa = {
-        "Nome Completo": lista[0],
-        "CPF": lista[1],
-        "Idade": lista[2],
-        "Sexo": lista[3],
-        "Sintomas": lista[4],
-        "Duração": lista[5],
-        "Alergia": lista[6],
-        "Medicação em uso": lista[7],
-        "Suspeitas": lista[8],
-        "Data de atendimento": datetime.now().date(),
-        "Horário de atendimento": f"{datetime.now().time()}".split(".")[0]
-    }
-    pdf = canvas.Canvas(f"Paciente - {lista[0]}.pdf")
-    contlinhas = 0
-    # Escrevendo primeiro pdf:
-    for element in informa:
-        pdf.drawString(100, 700 - contlinhas, f"{element}: {informa[element]}")
-        contlinhas += 20
-    pdf.save()
-    # trabalhando com segundo pdf:
-    stamp = PdfReader("aux/template.pdf").pages[0]
-    writer = PdfWriter(clone_from=f"Paciente - {lista[0]}.pdf")
-    for page in writer.pages:
-        page.merge_page(stamp, over=False)
-    writer.write(f"Paciente - {lista[0]}.pdf")
-    print("Ficha PDF gerada com sucesso.")
-
-
-def adicionar_usuario(nome, cpf, idade, sexo, sintomas, duracao, alergias, medicacao, suspeitas):
-    # Carregar dados existentes do arquivo JSON
-    with open('aux/usuarios.json', 'r', encoding='utf-8') as arquivo:
-        dados = json.load(arquivo)
-    # Adicionar novo usuário
-    novo_usuario = {
-        'Nome': nome,
-        'CPF': cpf,
-        'Idade': idade,
-        'Sexo': sexo,
-        'Sintomas': sintomas,
-        'Duração': duracao,
-        'Alergias': alergias,
-        'Medicação': medicacao,
-        'Suspeitas': suspeitas,
-        "Data de atendimento": f"{datetime.now().date()}",
-        "Horário de atendimento": f"{datetime.now().time()}".split(".")[0]
-    }
-
-    dados.append(novo_usuario)
-
-    # Salvar dados atualizados de volta ao arquivo JSON
-    with open('aux/usuarios.json', 'w', encoding='utf-8') as arquivo:
-        json.dump(dados, arquivo, indent=4, ensure_ascii=False)
-
-
-def buscar_usuario(doc: str) -> None:
+def serch_user_db(doc: str) -> None:
     # Procura o usuário a partir do seu CPF no arquivo JSON
     while True:
         with open(doc, "r", encoding='utf-8') as file:
-            usuarios = json.load(file)
-        pesquisa = input("Digite o CPF do paciente: ")
-        while len(pesquisa) != 11:
-            pesquisa = input("CPF inválido, por favor, insira apenas os números: ")
-        aux = False
-        ind = 0
-        pesquisa = f"{pesquisa[:3]}.{pesquisa[3:6]}.{pesquisa[6:9]}-{pesquisa[9:]}"
-        for i in range(len(usuarios)):
-            if pesquisa == usuarios[i]["CPF"]:
-                aux = True
-                ind = i
-        if aux:
-            limpar()
-            input(f'Nome: {usuarios[ind]["Nome"]}\nCPF: {usuarios[ind]["CPF"]}\n'
-                  f'Idade: {usuarios[ind]["Idade"]}\nSexo: {usuarios[ind]["Sexo"]}\n'
-                  f'Sintomas: {usuarios[ind]["Sintomas"]}\nDuração: {usuarios[ind]["Duração"]}\n'
-                  f'Alergias: {usuarios[ind]["Alergias"]}\nMedicações: {usuarios[ind]["Medicação"]}\n'
-                  f'Suspeitas: {usuarios[ind]["Suspeitas"]}\n'
-                  f'Data de atendimento: {usuarios[ind]["Data de atendimento"]}\n'
-                  f'Horário de atendimento: {usuarios[ind]["Horário de atendimento"]}\n'
+            users_list = json.load(file)
+        cpf_search = input("Digite o CPF do paciente: ")
+        while len(cpf_search) != 11:
+            cpf_search = input("CPF inválido, por favor, insira apenas os números: ")
+        found = False
+        index = 0
+        cpf_search = f"{cpf_search[:3]}.{cpf_search[3:6]}.{cpf_search[6:9]}-{cpf_search[9:]}"
+        for i in range(len(users_list)):
+            if cpf_search == users_list[i]["CPF"]:
+                found = True
+                index = i
+        if found:
+            clear()
+            input(f'Nome: {users_list[index]["Nome"]}\nCPF: {users_list[index]["CPF"]}\n'
+                  f'Idade: {users_list[index]["Idade"]}\nSexo: {users_list[index]["Sexo"]}\n'
+                  f'Sintomas: {users_list[index]["Sintomas"]}\nDuração: {users_list[index]["Duração"]}\n'
+                  f'Alergias: {users_list[index]["Alergias"]}\nMedicações: {users_list[index]["Medicação"]}\n'
+                  f'Suspeitas: {users_list[index]["Suspeitas"]}\n'
+                  f'Data de atendimento: {users_list[index]["Data de atendimento"]}\n'
+                  f'Horário de atendimento: {users_list[index]["Horário de atendimento"]}\n'
                   f'Aperte qualquer tecla para prosseguir...')
             break
         else:
-            limpar()
-            voltar = input("Usuário não encontrado, deseja tentar novamente? (S)im ou (N)ão? ")
-            while voltar.upper() not in ["SIM", "NAO", "NÃO", "S", "N"]:
-                voltar = input("Opção inválida. Deseja prosseguir? (S)im ou (N)ão? ")
-            if voltar.upper() in ["N", "NAO", "NÃO"]:
+            clear()
+            go_back = input("Usuário não encontrado, deseja tentar novamente? (S)im ou (N)ão? ")
+            while go_back.upper() not in ["SIM", "NAO", "NÃO", "S", "N"]:
+                go_back = input("Opção inválida. Deseja prosseguir? (S)im ou (N)ão? ")
+            if go_back.upper() in ["N", "NAO", "NÃO"]:
                 break
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from functions import *
 
 while True:
-    limpar()
+    clear()
     print(" ╔════════════════ MENU ══════════════════╗")
     print(" ║ 1 - Iniciar Atendimento                ║")
     print(" ║ 2 - Consultar Histórico                ║")
@@ -13,76 +13,80 @@ while True:
 
     match int(opc):
         case 0:
-            limpar()
+            clear()
             break
         case 1:
-            limpar()
+            clear()
             input("Olá, Sou HapBot uma inteligencia artificial feita para te ajudar nesse momento dificil!\nSeja "
                   "bem-vindo ao nosso hospital.\nVou te atender de forma rápida e eficiente.\nBasta responder algumas "
                   "perguntas simples. Vamos começar?\nAperte enter para prosseguirmos... ")
 
             while True:
-                limpar()
+                clear()
                 cpf = input("Primeiro, digite o seu CPF: ")
                 # Validar entrada: CPF
                 while len(cpf) != 11:
                     cpf = input("CPF inválido, por favor, insira apenas os números: ")
 
-                limpar()
-                nascimento = input("Agora digite a data do seu nascimento: ")
+                clear()
+                bday = input("Agora digite a data do seu nascimento: ")
                 # Validar entrada: Nascimento
-                while not validador_data(nascimento):
-                    nascimento = input("Data inválida, insira sua data de nascimento no formato dd/mm/aaaa ou "
-                                       "ddmmaaaa: ")
+                while not date_validator(bday):
+                    bday = input("Data inválida, insira sua data de nascimento no formato dd/mm/aaaa ou "
+                                 "ddmmaaaa: ")
 
                 # Verificar se o CPF e a data de nascimento constam na Receita Federal
-                data_dict = consultar_cpf(cpf, nascimento)
-                if data_dict['Situação'] == 'OK':
+                query_dict = cpf_query(cpf, bday)
+                if query_dict['Situação'] == 'OK':
+                    # Cria o objeto 'paciente' da classe 'Patient'
+                    paciente = Patient(cpf=query_dict["CPF"], name=query_dict["Nome"], age=query_dict["Idade"],
+                                       gender=query_dict["Sexo"])
                     break
                 else:
                     input("Os dados inseridos são inválidos! Tente novamente...")
 
-            limpar()
-            sintomas = input("Quais são os seus sintomas? ")
-            while len(sintomas) < 5:
-                sintomas = input("Descreva os seus sintomas: ")
+            # Começa a atribuir valores aos atributos padrões da Classe:
 
-            limpar()
-            duracao = input("Quanto tempo faz que você está passando por isso? ")
-            while len(duracao) < 5:
-                duracao = input("Descreva os seus sintomas: ")
+            clear()
+            paciente.symp = input("Quais são os seus sintomas? ")
+            while len(paciente.symp) < 5:
+                paciente.symp = input("Descreva os seus sintomas: ")
 
-            limpar()
-            alergico = input("Você é alérgico a alguma medicação? Se sim, qual? ")
+            clear()
+            paciente.duration = input("Quanto tempo faz que você está passando por isso? ")
+            while len(paciente.duration) < 4:
+                paciente.duration = input("Descreva os seus sintomas: ")
 
-            limpar()
-            medicacao = input("Está fazendo o uso de alguma medicação nesse momento? Se sim, qual? ")
+            clear()
+            paciente.allergies = input("Você é alérgico a alguma medicação? Se sim, qual? ")
 
-            limpar()
+            clear()
+            paciente.medication = input("Está fazendo o uso de alguma medicação nesse momento? Se sim, qual? ")
+
+            clear()
             input("Pronto! Vou te encaminhar para a próxima etapa com os nossos profissionais! "
                   "Pressione qualquer tecla...")
 
-            # Cria texto com as informações para a IA
-            userdata = (f"Idade: {data_dict['Idade']}\nSexo: {data_dict['Sexo']}\nSintomas: {sintomas}\n"
-                        f"Duração: {duracao}\n Alergias: {alergico}\n Medicamento em uso: {medicacao}")
-            
             print("\nSalvando...")
-            suspeitas = gpt_ask(userdata)
 
-            create_pdf([data_dict['Nome'], data_dict['CPF'], data_dict['Idade'], data_dict['Sexo'], sintomas, duracao,
-                        alergico, medicacao, suspeitas])
+            # Pesquisa e atribui as suspeitas ao paciente
+            paciente.get_suspicion()
 
-            adicionar_usuario(data_dict['Nome'], data_dict['CPF'], data_dict['Idade'], data_dict['Sexo'], sintomas,
-                              duracao, alergico, medicacao, suspeitas)
+            # Adiciona o usuário ao arquivo JSON
+            paciente.add_user_db()
+
+            # Cria a ficha de usuário em PDF
+            paciente.create_pdf()
+
         case 2:
-            limpar()
+            clear()
             opt = input("Essa é uma função exclusiva de administradores! Deseja prosseguir? (S)im ou (N)ão? ")
             while opt.upper() not in ["SIM", "NAO", "NÃO", "S", "N"]:
                 opt = input("Opção inválida. Deseja prosseguir? (S)im ou (N)ão? ")
             if opt.upper() in ["SIM", "S"]:
-                senha = input("Digite sua senha ('N' para sair): ")
-                while senha != "admin" and senha.upper() not in ["N", "NAO", "NÃO"]:
-                    senha = input("Senha incorreta! Tente novamente ('N' para sair): ")
-                if senha == "admin":
-                    buscar_usuario("usuarios.json")
+                password = input("Digite sua senha ('N' para sair): ")
+                while password != "admin" and password.upper() not in ["N", "NAO", "NÃO"]:
+                    password = input("Senha incorreta! Tente novamente ('N' para sair): ")
+                if password == "admin":
+                    serch_user_db("aux/usuarios.json")
                             
